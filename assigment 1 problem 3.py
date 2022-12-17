254fbd5fb7f81506feac2b97f6f0711ca67c91a6
akshay-0911 <120121323+akshay-0911@users.noreply.github.com>
2022-12-17 23:34:36 +0530

    Add files via upload

---
+++ assigment 1 problem 3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+l = [] 
+
+even, odd= 0, 0
+
+for i in l: 
+
+    if i % 2 == 0: 
+
+        even += 1
+
+    else: 
+
+        odd+= 1          
+
+print("Even : ", even) 
+
+print("Odd : ", odd)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
